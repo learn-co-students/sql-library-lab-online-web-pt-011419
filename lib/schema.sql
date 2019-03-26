7e7e853672ee58f5a9e3cf238fc5cec2c289601d
Kelsey Low <kklowlow@gmail.com>
2019-03-26 04:07:51 +0000

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,25 +1,20 @@
-CREATE TABLE series (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  author_id INTEGER,
-  sub_genre_id INTEGER
-);
-
-CREATE TABLE sub_genre (
-  id INTEGER PRIMARY KEY,
-  name TEXT
-);
-
-CREATE TABLE authors (
-  id INTEGER PRIMARY KEY,
-  name TEXT
-);
-
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
 );
 
 CREATE TABLE characters (
@@ -31,7 +26,12 @@ CREATE TABLE characters (
   series_id INTEGER
 );
 
-CREATE TABLE character_books ( 
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
   character_id INTEGER,
   book_id INTEGER

d4b2e0a40d33187989c43eeb55265cf5a81e2f8a
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-01 01:21:12 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,25 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES
-("Harry Potter", 1, 1),
-("Hunger Games", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+(1, "Harry Potter", 1, 1),
+(2, "Hunger Games", 2, 2);
+
+INSERT INTO authors (id, name) VALUES
+(1, "J.K. Rowling")
+(2, "Suzanne Collins");
+
+INSERT INTO books (id, title, year, series_id) VALUES
+(1, "The Philosopher's Stone", 1997, 1),
+(2, "The Chamber of Secrets", 1998, 1),
+(3, "The Prisoner of Azkaban", 1999, 1),
+(4, "The Hunger Games", 2008, 2),
+(5, "Catching Fire", 2009, 2),
+(6, "Mockingjay", 2010, 2);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
+(1, "Harry Potter", "There are all kinds of courage", "human", )
+(2,)
+(3,)
+(4,)
+(5,)
+(6,)
+(7,)
+(8,)
